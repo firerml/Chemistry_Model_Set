a828968f03b9fd9f1dfdce592093d1c77caccd45
firerml <firerml@gmail.com>
2015-02-26 00:21:35 -0500

    commented saveLoadMolecule.js

--- app/assets/javascripts/saveLoadMolecule.js
+++ app/assets/javascripts/saveLoadMolecule.js
@@ -1,7 +1,9 @@
 function loadMolecule(molecule) {
+  // parse from string to array
   var instructions = JSON.parse(molecule.instructions);
   App.instructions = [];
   clearScreen();
+  // follow the steps, rebuilding the molecule piece by piece
   for (var i = 0; i < instructions.length; i++) {
     loadPiece(instructions[i]);
   }
@@ -11,18 +13,25 @@ function loadPiece(step) {
   switch (step[0]) {
     case 'add atom':
       var bondHead;
+      // which atom type to place
       var cursorID = step[1];
+      // atom's parent bond
       var bondID = step[2];
+      // there's no bondID, and therefore no bondHead, if it's the first atom
       if (bondID || bondID == 0) bondHead = App.bonds[bondID].children[0];
       addAtom(cursorID, bondHead);
       break;
     case 'add bond':
+      // bond's parent atom
       var atomID = step[1];
       var atom = App.atoms[atomID];
+      // hole on the atom this bond was placed in
       var holeNum = step[2];
       addSingleBond(atom,holeNum);
       break;
     case 'upgrade bond':
+      // only bonds between atoms can be upgraded,
+      // so they have a child atom and a parent atom
       var bond = App.bonds[step[1]];
       var childAtom = App.atoms[step[2]];
       var parentAtom = App.atoms[step[3]];
@@ -37,13 +46,16 @@ function loadPiece(step) {
 function saveMolecule(name) {
   // save the rotations of bonds
   for (var i = 0; i < App.bonds.length; i++) {
+    // only single bonds can be rotated, so only save rotation of single bonds
     if (App.bonds[i].children[1].userData.pieceName === 'single bond body') {
       App.instructions.push(['rotate bond',i,App.bonds[i].userData.rotation]);
     }
   }
 
+  // array -> string to be stored in database
   var instructionsJSON = JSON.stringify(App.instructions);
 
+  // preparing data for ajax post request
   var newMolecule = {
     molecule: {
       name: name,
@@ -52,6 +64,7 @@ function saveMolecule(name) {
   };
 
   $.post('/molecules', newMolecule, function(res) {
+    // Add the new molecule to the load list
     $('ul').append($('<li>').attr('id',res.id).text(res.name));
   });
 }
